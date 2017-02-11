fa26dd6e57e080bc79d93fe1f1e6edb96a569410
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-02-11 14:22:25 +0000

    Add random username generator
    The adjectives and the nouns are added to an array. A random word is taken
    from both arrays, and these are printed together. This is done 6 times.

---
+++ random_username.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from random import randint
+
+def find_word(filename):
+    file = open(filename,"r")
+    list_of_words = file.readlines()
+    return(list_of_words[randint(0, len(list_of_words) - 1)].rstrip())
+    file.close()
+
+for word in range(6):
+    adjective = (find_word("./wordlists/wordlist_adjectives.txt"))
+    noun = (find_word("./wordlists/wordlist_nouns.txt"))
+    print((adjective[0].upper() + adjective[1:] + noun[0].upper() + noun[1:]))
