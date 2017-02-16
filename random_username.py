72c2a64b5c6b602679de9e221584dd0bf1afc8fa
Awes Mubarak <awes.mubarak@awesmubarak.com>
2017-02-16 16:46:42 +0000

    Allow command line argument parsing
    The user can now modify certain aspects of the program. Currently, the
    number of usernames generated can be changed to any positive value and
    underscores can be used to seperate the adjective from the noun, rather
    than forcing the use of camel case.
    
    Resolves: #1 (Allow underscores alongside camel case)

--- random_username.py
+++ random_username.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import sys, getopt
 from random import randint
 
 def find_word(filename):
@@ -8,10 +9,45 @@ def find_word(filename):
     return(list_of_words[randint(0, len(list_of_words) - 1)].rstrip())
     file.close()
 
-print("")
-print("Random usernames:")
-for word in range(6):
-    adjective = (find_word("./wordlists/adjectives.txt"))
-    noun = (find_word("./wordlists/nouns.txt"))
-    print("    ", (adjective[0].upper() + adjective[1:] + noun[0].upper() + noun[1:]))
-print("")
+def main(args):
+
+    USAGE_INSTRUCTIONS = "\n random_username.py -u -n <number_of_usernames> \n"
+    number_of_usernames = 6
+    includes_underscores = False
+
+    try:
+        opts, args = getopt.getopt(args,"hun:",["help","underscores=","number_of_usernames=",])
+    except getopt.GetoptError:
+        print(USAGE_INSTRUCTIONS)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "help"):
+            print(USAGE_INSTRUCTIONS)
+            sys.exit()
+        elif opt in ("-n", "--number_of_usernames"):
+            try:
+                if int(arg) > 0:
+                    number_of_usernames = int(arg)
+                else:
+                    print(USAGE_INSTRUCTIONS)
+                    sys.exit()
+            except:
+                print(USAGE_INSTRUCTIONS)
+                sys.exit()
+        elif opt in ("-u", "--underscores="):
+            includes_underscores = True
+
+    print("\n Your usernames:")
+    for word_number in range(number_of_usernames):
+        adjective = (find_word("./wordlists/adjectives.txt"))
+        noun = (find_word("./wordlists/nouns.txt"))
+        if includes_underscores:
+            print("    ", (adjective + "_" + noun))
+        else:
+            print("    ", (adjective[0].upper() + adjective[1:] +
+                           noun[0].upper() + noun[1:]))
+    print("")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
